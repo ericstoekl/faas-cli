e90747ec4b0702109acb64eb44638c7620bc9b21
Eric Stoekl <ems5311@gmail.com>
2017-09-06 23:12:34 -0700

    Restructuring of add-template by Minh-Quan
    1. add-template updates
    2. overwrite functionality
    3. cacheLanguages for faster parsing, cache directory for zip file
    4. tests for add-template, with overwrite option
    5. test for PullTemplates function
    1) Enable github repo ending in '/'
    2) Allow 'faas-cli add-template' (no need to specify repo), picks default repo
    3) More concise (shorter) summary of language templates applied/not applied
    4) Change validTemplate to public facing IsValidTemplate function in stack package
    5) Refactor BuildImage() function to use IsValidTemplate to check if language is valid (rather than hard-coded values)
    6) Remove --lang option from invoke command (it is not used)
    
    Signed-off-by: Eric Stoekl <ems5311@gmail.com>
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- commands/build.go
+++ commands/build.go
@@ -68,7 +68,7 @@ func runBuild(cmd *cobra.Command, args []string) {
 
 	var services stack.Services
 	if len(yamlFile) > 0 {
-		parsedServices, err := stack.ParseYAMLFile(yamlFile, regex, filter)
+		parsedServices, err := stack.ParseYAMLFileForStack(yamlFile, regex, filter)
 		if err != nil {
 			log.Fatalln(err.Error())
 			return
@@ -79,7 +79,7 @@ func runBuild(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	if pullErr := PullTemplates(""); pullErr != nil {
+	if pullErr := PullTemplates(); pullErr != nil {
 		log.Fatalln("Could not pull templates for OpenFaaS.", pullErr)
 	}
 
@@ -142,13 +142,13 @@ func build(services *stack.Services, queueDepth int) {
 }
 
 // PullTemplates pulls templates from Github from the master zip download file.
-func PullTemplates(templateUrl string) error {
+func PullTemplates() error {
 	var err error
 	exists, err := os.Stat("./template")
 	if err != nil || exists == nil {
 		log.Println("No templates found in current directory.")
 
-		err = fetchTemplates(templateUrl)
+		err = fetchTemplates("", false)
 		if err != nil {
 			log.Println("Unable to download templates from Github.")
 			return err
